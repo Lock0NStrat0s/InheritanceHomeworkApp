434d188f383ce064861cca2fa44be5b39a3acaf6
Mohammad Jabir <mohammadj94@gmail.com>
2023-09-17 15:56:33 -0600

    Add project files.

---
+++ InheritanceHomework/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace InheritanceHomework;
+
+// create program that allows you to either buy or rent
+// different types of cars
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        Console.ReadLine();
+    }
+}
+
+public interface IEngine
+{
+    void Run();
+}
+
+public class Vehicle
+{
+    public string Name { get; set; }
+}
+
+public class Car : Vehicle
+{
+    
+}
+
+public class Truck : Vehicle
+{
+
+}
+
+public class Motorcycle : Vehicle
+{
+
+}
